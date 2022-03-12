af60ad0153bbf094e62ca789510cb23f0ca7e30c
Rafael Galdino <galdineris@gmail.com>
2022-03-12 12:18:46 -0300

    Make Project Setup

---
+++ GithuBrowser/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  GithuBrowser
+//
+//  Created by Rafael Galdino on 12/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
